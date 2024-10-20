4789ebb7b71525c6ea1ae4556b1a937a8c21c79c
Rupin Raveendra Nath <rupinr@live.com>
2024-10-20 12:04:16 +0200

    move site selector

--- src/ScheduleSelector.tsx
+++ src/ScheduleSelector.tsx
@@ -1,28 +1,23 @@
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import React, { useState } from 'react'
-import FormControlLabel from '@mui/material/FormControlLabel';
-import { useEffect } from 'react'
-import Autocomplete from '@mui/material/Autocomplete';
-import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
-import { getSites, updatePreference, getSubscription } from './service/service'
-import { useLocation, useSearchParams } from 'react-router-dom'
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid2';
+import { useSearchParams } from 'react-router-dom';
+import { getSites, updatePreference, getSubscription } from './service/service';
 import DaySelector from './DaySelector';
-import TimeSlotSelector from './TimeSlotSelector'
+import TimeSlotSelector from './TimeSlotSelector';
+import SiteSelector, { Option } from './SiteSelector';
 
 const ScheduleSelector = () => {
-
-    const [searchParams] = useSearchParams()
-    const token = searchParams.get('authToken')
-    const [timeSlot, setTimeSlot] = useState('Monday')
-    const [options, setOptions] = useState<{ label: string, value: string }[]>([])
-    const [defaultOptions, setDefaultOptions] = useState<{ label: string, value: string }[]>([])
+    const [searchParams] = useSearchParams();
+    const token = searchParams.get('authToken');
+    const [timeSlot, setTimeSlot] = useState('');
+    const [options, setOptions] = useState<Option[]>([]);
+    const [defaultOptions, setDefaultOptions] = useState<Option[]>([]);
+    const [selectedSites, setSelectedSites] = useState<Option[]>([]);
     const [daySelection, setDaySelection] = useState<{ [key: string]: boolean }>({
         monday: true,
         tuesday: true,
@@ -31,126 +26,82 @@ const ScheduleSelector = () => {
         friday: true,
         saturday: true,
         sunday: true
-    })
+    });
 
-    const [text, setText] = useState('');
-    const [filteredOptions, setFilteredOptions] = useState<{ label: string, value: string }[]>([]);
-
-
-    const handleDayChangeFor = (updatedDays: any) => {
+    const handleDayChangeFor = (updatedDays: { [key: string]: boolean }) => {
         setDaySelection(updatedDays);
     };
 
-    const handleTimeSlotChangeFor = (updateTimeSlot: any) => {
-        console.log(updateTimeSlot)
+    const handleTimeSlotChangeFor = (updateTimeSlot: string) => {
         setTimeSlot(updateTimeSlot);
     };
 
-    const fetchOptions = async () => {
-        const data = await getSites()
-        const destinations: any[] = data.map((source: any) => ({
-            label: source.url,
-            value: source.url,
-        }));
-        setOptions(destinations)
-    }
-    const fetchExistingData = async () => {
-        const subscription = await getSubscription(token!!)
-        let existingSites = []
-        console.log("Sitesss", subscription)
-        if (subscription.sites != null) {
-            existingSites = subscription.sites.map((source: any) => ({
-                label: source,
-                value: source,
-            }));
-        }
-        console.log(subscription)
-        setDefaultOptions(existingSites)
-        setDaySelection(subscription.subscriptionSchedule.dailyFrequency)
-        setTimeSlot(subscription.subscriptionSchedule.timeSlot)
-    }
+    const handleSiteChange = (updatedSites: Option[]) => {
+        setSelectedSites(updatedSites);
+    };
 
+    const fetchData = async () => {
+        if (!token) return;
+        const subscription = await getSubscription(token);
+        const existingSites: Option[] = subscription.sites.map((source: string) => ({
+            label: source,
+            value: source,
+        }));
+        const sites = await getSites();
+        const options: Option[] = sites.map((item: { url: string }) => ({
+            label: item.url,
+            value: item.url
+        }));
+        setOptions(options);
+        setDefaultOptions(existingSites);
+        setSelectedSites(existingSites);
+        setDaySelection(subscription.subscriptionSchedule.dailyFrequency);
+        setTimeSlot(subscription.subscriptionSchedule.timeSlot);
+    };
 
     useEffect(() => {
-        fetchExistingData()
-        fetchOptions()
-    }, [])
-
-
-    const handleSites = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const value = event.target.value;
-        setText(value);
-        setFilteredOptions(options.filter(option =>
-            option.label.toLowerCase().includes(value.toLowerCase())
-        ));
-    };
+        fetchData();
+    }, [token]);
 
     const handleSubmit = () => {
         const data = {
             confirmed: true,
             subscriptionSchedule: {
-                dailyFrequency: {
-                    monday: daySelection['monday'],
-                    tuesday: daySelection['tuesday'],
-                    wednesday: daySelection['wednesday'],
-                    thursday: daySelection['thursday'],
-                    friday: daySelection['friday'],
-                    saturday: daySelection['saturday'],
-                    sunday: daySelection['sunday'],
-                },
+                dailyFrequency: { ...daySelection },
                 timeSlot: timeSlot,
                 timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
             },
-            sites: options.map((item) => item.value)
-
-        }
-        updatePreference(data, token!!)
-    }
+            sites: selectedSites.map((item) => item.value)
+        };
+        updatePreference(data, token!);
+    };
 
     return (
-        (
-            <Container maxWidth="md">
-                <Box sx={{ my: 4 }}>
-
-                    <Typography component="p" sx={{ mb: 2 }}>
-                        Thank you for subscribing! To tailor your news experience, please select the days of the week and the preferred time slot for your email delivery. Whether you prefer a morning digest to start your day, an afternoon update, or an evening recap, we’ve got you covered.
-                    </Typography>
-                    <Typography variant="h6" sx={{ mb: 2 }}>
-                        Select Days of the Week:
-                    </Typography>
-                    <Grid container spacing={2}>
-                        <DaySelector initialDays={daySelection} onDayChange={handleDayChangeFor} />
-                    </Grid>
-                    <Grid container spacing={2}>
-                        <TimeSlotSelector initialTimeSlot={timeSlot} onTimeSlotChange={handleTimeSlotChangeFor} />
-                    </Grid>
+        <Container maxWidth="md">
+            <Box sx={{ my: 4 }}>
+                <Typography component="p" sx={{ mb: 2 }}>
+                    Thank you for subscribing! To tailor your news experience, please select the days of the week and the preferred time slot for your email delivery. Whether you prefer a morning digest to start your day, an afternoon update, or an evening recap, we’ve got you covered.
+                </Typography>
+                <Typography variant="h6" sx={{ mb: 2 }}>
+                    Select Days of the Week:
+                </Typography>
+                <Grid container spacing={2}>
+                    <DaySelector initialDays={daySelection} onDayChange={handleDayChangeFor} />
+                </Grid>
+                <Grid container spacing={2}>
+                    <TimeSlotSelector initialTimeSlot={timeSlot} onTimeSlotChange={handleTimeSlotChangeFor} />
+                </Grid>
+                <Grid container spacing={2}>
                     <Stack spacing={3} sx={{ width: '100%', mt: 3 }}>
-                        <Autocomplete
-                            multiple
-                            id="tags-outlined"
-                            options={options}
-                            value={defaultOptions}
-                            onChange={(event, newValue) => setDefaultOptions(newValue)}
-                            getOptionLabel={(option) => option.label}
-                            filterSelectedOptions
-                            renderInput={(params) => (
-                                <TextField
-                                    {...params}
-                                    label="Sites"
-                                    placeholder="Sites"
-                                    onChange={handleSites}
-                                />
-                            )}
-                        />
+                        <SiteSelector options={options} defaultOptions={defaultOptions} onSiteChange={handleSiteChange} />
                     </Stack>
-                    <Button variant="contained" color="primary" onClick={handleSubmit} sx={{ mt: 4 }}>
-                        Save Preferences
-                    </Button>
-                </Box>
-            </Container>
-
-        )
-    )
+                </Grid>
+                <Button variant="contained" color="primary" onClick={handleSubmit} sx={{ mt: 4 }}>
+                    Save Preferences
+                </Button>
+            </Box>
+        </Container>
+    );
 };
 
-export default ScheduleSelector
+export default ScheduleSelector;
---
+++ src/SiteSelector.tsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+
+interface SiteSelectorProps {
+    options: Option[];
+    defaultOptions: Option[];
+    onSiteChange: (updatedSites: Option[]) => void;
+}
+
+export interface Option {
+    label: string;
+    value: string;
+}
+
+const SiteSelector: React.FC<SiteSelectorProps> = ({ options, defaultOptions, onSiteChange }) => {
+    const [selectedOptions, setSelectedOptions] = useState<Option[]>(defaultOptions);
+    const [allOptions, setAllOptions] = useState<Option[]>(options);
+
+    useEffect(() => {
+        setAllOptions(options);
+    }, [options]);
+
+    useEffect(() => {
+        setSelectedOptions(defaultOptions);
+    }, [defaultOptions]);
+
+
+    const handleUpdate = (event: any, newValue: Option[]) => {
+
+
+        setSelectedOptions(newValue);
+        onSiteChange(newValue);
+        console.log('handleUpdate')
+        console.log('allOptions', allOptions)
+    };
+
+    return (
+        <div>
+            <Autocomplete
+                multiple
+                id="tags-outlined"
+                options={allOptions}
+                value={selectedOptions}
+                getOptionLabel={(option) => option.label}
+                onChange={handleUpdate}
+                filterSelectedOptions
+                renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        label="Sites"
+                        placeholder="Sites"
+                    />
+                )}
+            />
+        </div>
+    );
+};
+
+export default SiteSelector;
