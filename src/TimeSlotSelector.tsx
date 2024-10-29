5beeb9744ba33813196a343421913332e6742776
Rupin Raveendra Nath <rupinr@live.com>
2024-10-29 21:12:57 +0100

    add hints

--- src/TimeSlotSelector.tsx
+++ src/TimeSlotSelector.tsx
@@ -21,11 +21,29 @@ const TimeSlotSelector: React.FC<TimeSlotSelectorProps> = ({ initialTimeSlot, on
         onTimeSlotChange(event.target.value)
     }
 
+
     const [timeSlot, setTimeSlot] = useState('Morning')
 
     useEffect(() => {
         setTimeSlot(initialTimeSlot);
     }, [initialTimeSlot]);
+
+    function formatTimeRange(startTime: number, endTime: number): string {
+        const userLocale = navigator.language || 'en-US';
+        const options: Intl.DateTimeFormatOptions = { hour: 'numeric', minute: 'numeric', hour12: true };
+        const startFormatted: string = new Intl.DateTimeFormat(userLocale, options).format(startTime);
+        const endFormatted: string = new Intl.DateTimeFormat(userLocale, options).format(endTime);
+        return `${startFormatted} - ${endFormatted}`;
+    }
+
+    const date: Date = new Date();
+    const six = date.setHours(6, 0, 0, 0);
+    const twelwe = date.setHours(12, 0, 0, 0);
+    const eighteen = date.setHours(18, 0, 0, 0);
+    const twenty = date.setHours(20, 0, 0, 0);
+
+    date.setHours(12, 0);
+
 
     return (
         <div>
@@ -33,10 +51,10 @@ const TimeSlotSelector: React.FC<TimeSlotSelectorProps> = ({ initialTimeSlot, on
                 Select Time Slot:
             </Typography>
             <RadioGroup value={timeSlot} onChange={handleTimeSlotChange}>
-                <FormControlLabel value="Morning" control={<Radio />} label="Morning" />
-                <FormControlLabel value="Afternoon" control={<Radio />} label="Afternoon" />
-                <FormControlLabel value="Evening" control={<Radio />} label="Evening" />
-                <FormControlLabel value="Night" control={<Radio />} label="Night" />
+                <FormControlLabel value="Morning" control={<Radio />} label={`Morning (${formatTimeRange(six, twelwe)})`} />
+                <FormControlLabel value="Afternoon" control={<Radio />} label={`Afternoon (${formatTimeRange(twelwe, eighteen)})`} />
+                <FormControlLabel value="Evening" control={<Radio />} label={`Evening (${formatTimeRange(eighteen, twenty)})`} />
+                <FormControlLabel value="Night" control={<Radio />} label={`Night (${formatTimeRange(twenty, six)})`} />
             </RadioGroup>
         </div>
     );
